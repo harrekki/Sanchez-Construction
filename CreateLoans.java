d371355b9495a236a90518fde3adfd9b0a789504
David LaRocco <dlarocco77@gmail.com>
2022-03-28 17:34:29 -0500

    added write to CSV option to CreateLoans.java

--- CreateLoans.java
+++ CreateLoans.java
@@ -1,13 +1,16 @@
 // filename:    CreateLoans.java
 // Developer:   David LaRocco
 // Date:        March 14, 2022
-// Description: This applicaton that displays loan information for the Sanchez Construction Co. It prompts the user
-//              to enter the current prime interest rate and other information about 5 new loans.  It then generates 
-//              a loan number and calculates the total amount the customer owes at the end of each loan term.  The 
-//              details of each loan are then displayed.
+// Description: This application displays loan information for the Sanchez Construction Company. It prompts the user
+//              to enter the current prime interest rate and other information about new loans until the user quits
+//              the program.  It then generates a loan number and calculates the total amount the customer owes at 
+//              the end of each loan term. The results can be either written to a CSV file or displayed on screen.
+
 import java.util.ArrayList;
 import java.text.DecimalFormat;
 import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
 
 public class CreateLoans 
 {
@@ -17,9 +20,9 @@ public class CreateLoans
         ArrayList<Loan> loans = new ArrayList<Loan>();
         Scanner input = new Scanner(System.in);
         DecimalFormat dfDollar = new DecimalFormat("###,##0.00"); // to display dollar values with 2 decimal places
-        double primeInterestRate = 0.0325;
-
-        // Variables for loop
+        
+        // Variables for while loop 
+        double primeInterestRate = 0;
         int userChoice = 0;
         final int QUIT = 999;
         String userLastName = "";
@@ -31,6 +34,7 @@ public class CreateLoans
         System.out.print("Loan Type:\n\t1 - Business\n\t2 - Personal\n Choose type of loan or " + QUIT + " to quit >> ");
         userChoice = input.nextInt();
         input.nextLine();
+
         // while loop to get information on each loan
         while(userChoice != QUIT)
         {
@@ -38,8 +42,8 @@ public class CreateLoans
             userLastName = input.nextLine();
             System.out.print("Enter loan amount >> ");
             userLoanAmount = input.nextDouble();
-            System.out.print("Loan Terms:\n\tShort-Term (1 year) - 1\n\tMedium-Term (3 year) - 3" +
-                                "\n\tLong-term (5 year) - 5\n Choose loan term >> ");
+            System.out.print("Loan Terms:\n\t1 - Short-Term (1 year)\n\t3 - Medium-Term (3 year)" +
+                                "\n\t5 - Long-term (5 year)\nChoose loan term >> ");
             userLoanTerm = input.nextInt();
 
             if(userChoice == 1)
@@ -54,28 +58,49 @@ public class CreateLoans
             input.nextLine();
         } // end while loop
         
-        // Display information on all loans
+        // Retrieve information on all loans
         System.out.println("\nNumber of loans entered: " + loans.size());
         System.out.println();
-        for(Loan application : loans)
+
+        // Ask user if they want output to CSV or screen
+        System.out.print("\t1 - CSV file\n\t2 - On-screen report" +
+                        "\nChoose output type >> ");
+        userChoice = input.nextInt();
+        input.nextLine();
+
+        if(userChoice == 1) // CSV file
         {
-            System.out.println(application.toString()); // Displays loan information
-            
-            // get loan amount, interest rate, and term from current application 
-            double loanAmount = application.getLoanAmount();
-            double interestRate = application.getInterestRate();
-            int loanTerm = application.getLoanTerm();
+            System.out.print("Enter file name (.csv extension will be added automatically) >> ");
+            String fileName = input.nextLine();
 
-            if(application.getLoanAmount() > 0)
-                // Calculate and display total amount due at end of the loan
-                System.out.println("Total amount due at end of loan term: $" + 
-                    dfDollar.format(calculateAmountDue(loanAmount, loanTerm, interestRate)));
-            else 
-                // Print error message
-                System.out.println("Loan denied - Maximum loan amount exceeded.");    
-            System.out.println("--------------------");
+            writeToCSVFile(fileName, loans);
         }
+        else // On-screen report
+        {
+            // Default to on-screen report if user selects invalid option
+            if(userChoice != 2)
+                System.out.println("Invalid entry - results will be displayed on screen.");
+            
+            for(Loan application : loans)
+            {
+                System.out.println(application.toString()); // Displays loan information
+                
+                // get loan amount, interest rate, and term from current application 
+                double loanAmount = application.getLoanAmount();
+                double interestRate = application.getInterestRate();
+                int loanTerm = application.getLoanTerm();
+    
+                if(application.getLoanAmount() > 0)
+                    // Calculate and display total amount due at end of the loan
+                    System.out.println("Total amount due at end of loan term: $" + 
+                        dfDollar.format(calculateAmountDue(loanAmount, loanTerm, interestRate)));
+                else 
+                    // Print error message
+                    System.out.println("Loan denied - Maximum loan amount exceeded.");    
+                System.out.println("--------------------");
+            }
 
+        }
         input.close();
     } // end of main method
    
@@ -87,6 +112,42 @@ public class CreateLoans
         {
             totalDue += totalDue * (interest / 100);
         }
+        totalDue = Math.round(totalDue * 100) / 100;
         return totalDue;
+    }
+
+    // method to create CSV file from loan application data
+    public static void writeToCSVFile(String file, ArrayList<Loan> arr) 
+    {
+        String fileName = file + ".csv";
+
+        try (PrintWriter pw = new PrintWriter(fileName)) 
+        {
+            // Column Headers
+            pw.write("LoanNumber,LastName,LoanAmount,LoanTerm,InterestRate,AmountDue,");
+            pw.println();
+            for(Loan application : arr) 
+            {
+                pw.write(application.getLoanNumber() + ",");
+                pw.write(application.getLastName() + ",");
+                pw.write(application.getLoanAmount() + ",");
+                pw.write(application.getLoanTerm() + ",");
+                pw.write(application.getInterestRate() + ",");
+
+                // get loan amount, interest rate, and term from current application 
+                double loanAmount = application.getLoanAmount();
+                double interestRate = application.getInterestRate();
+                int loanTerm = application.getLoanTerm();
+                pw.write(calculateAmountDue(loanAmount, loanTerm, interestRate) + ",");
+                pw.println();
+            }
+
+            System.out.println(fileName + " was created successfully");
+        }
+        catch (FileNotFoundException e) 
+        {
+            System.out.println("Error creating/ writing to file.");
+            e.printStackTrace();
+        }
     }
 }
